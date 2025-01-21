48819748aef423cf4be4ed44fe9b2599c761fcbb
rachraines <rachraines97@gmail.com>
2025-01-21 12:17:38 -0500

    window class completed

--- window_class.py
+++ window_class.py
@@ -7,10 +7,26 @@ class Window:
         self.root.title("Maze Solver") # Set the title of the window
 
         # Create a Canvas widget
-        self.canvas = (self.root, width, height, background="black")
-        self.canvas.pack(fill=Both, expand=True) # Pack the canvas so it can be drawn
+        self.canvas = Canvas(self.root, width, height, background="black")
+        self.canvas.pack(fill=BOTH, expand=True) # Pack the canvas so it can be drawn
 
         # Represents the running state
         self.running = False
 
-        
+        # Stops the program from running when window is closed
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    # Window will redraw itself when function is called
+    def redraw(self):
+        self.update_idletasks()
+        self.update()
+
+    # Coninues to draw window until it is closed
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    # Closes window
+    def close(self):
+        self.running = False
