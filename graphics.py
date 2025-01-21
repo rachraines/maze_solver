0c765988bad50f42803eebfd0d1f45d4a0253305
rachraines <rachraines97@gmail.com>
2025-01-21 13:40:18 -0500

    Line and Point Classes made

--- graphics.py
+++ graphics.py
@@ -31,3 +31,24 @@ class Window:
     # Closes window
     def close(self):
         self.__running = False
+
+
+    # Draws a line in the canvas
+    def draw_line(self, line, fill_color="white"):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.start = point1
+        self.end = point2
+
+    # Draws line on a given canvas
+    def draw(self, canvas, fill_color="white"):
+        canvas.create_line(
+            self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
+        )
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from window_class import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(50, 50), Point(100, 100))
+    win.draw_line(line1)
     win.wait_for_close()
 
 main()
