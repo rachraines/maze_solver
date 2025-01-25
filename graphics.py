8cfb9af59c002618c19aaca80eaa684c00ae705b
rachraines <rachraines97@gmail.com>
2025-01-25 11:05:11 -0500

    Cell Class created with draw() method

--- graphics.py
+++ graphics.py
@@ -52,3 +52,25 @@ class Line:
         canvas.create_line(
             self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2
         )
+
+class Cell:
+    def __init__(self, x1, x2, y1, y2, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+
+    def draw(self, canvas, fill_color="white"):
+        if self.has_left_wall:
+            canvas.create_line(self._x1, self._y1, self._x1, self._y2, fill=fill_color)
+        if self.has_top_wall:
+            canvas.create_line(self._x1, self._y1, self._x2, self._y1, fill=fill_color)
+        if self.has_right_wall:
+            canvas.create_line(self._x2, self._y1, self._x2, self._y2, fill=fill_color)
+        if self.has_bottom_wall:
+            canvas.create_line(self._x1, self._y2, self._x2, self._y2, fill=fill_color)
--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 def main():
+    # Create a window with dimensions 800x600
     win = Window(800, 600)
-    line1 = Line(Point(50, 50), Point(100, 100))
-    win.draw_line(line1)
+    
+    # Acccess the canvas from the window object
+    canvas = win._Window__canvas
+
+    #line1 = Line(Point(50, 50), Point(100, 100))
+    #win.draw_line(line1)
+   
+    cell1 = Cell(50, 100, 50, 100, win)
+    cell1.draw(canvas)
+    
+    cell2 = Cell(110, 160, 50, 100, win)
+    cell2.has_top_wall = False
+    cell2.draw(canvas)
+
+    cell3 = Cell(170, 220, 50, 100, win)
+    cell3.has_left_wall = False
+    cell3.has_bottom_wall = False
+    cell3.draw(canvas)
+
+    cell4 = Cell(230, 280, 50, 100, win)
+    cell4.has_top_wall = False
+    cell4.has_left_wall = False
+    cell4.has_bottom_wall = False
+    cell4.draw(canvas)
+
     win.wait_for_close()
 
 main()
